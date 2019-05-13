7cf48f1ff1e260f373e14d7a510e5f577bff2ed9
Nivedita Bijlani <nivedita.v.bijlani@gmail.com>
2019-05-13 12:11:35 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1uFD4ekwTkc54uU9eQbA4fbrsREMKxN1K'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['chickenpox','scarletfever','measles']
 path = Path(__file__).parent
 
 app = Starlette()
