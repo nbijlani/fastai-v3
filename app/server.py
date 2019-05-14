24aea4c11263df5382cc73248557471b9630bd4d
Nivedita Bijlani <nivedita.v.bijlani@gmail.com>
2019-05-14 13:40:53 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/rwxald6buqe0ebl/export.pkl?dl=0'
+export_file_url = 'https://drive.google.com/open?id=1fkCN8vjGSb5cMJaAxGJVE0MTvcID2wQf'
 export_file_name = 'export.pkl'
 
 classes = ['chickenpox','scarletfever','measles']
