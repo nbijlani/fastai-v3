f0cffba0aa7e0d09804a09f53121b3bc0b62f7f7
Nivedita Bijlani <nivedita.v.bijlani@gmail.com>
2019-05-14 15:56:48 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/m5yo239lt9wb06m/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/m5yo239lt9wb06m/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['chickenpox','scarletfever','measles']
